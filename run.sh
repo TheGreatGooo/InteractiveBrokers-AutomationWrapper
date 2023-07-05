d8f4c0c7815793f9a9281a3d07702fb856dcec0f
TheGreatGooo <thegreatgooo@gmail.com>
2023-07-05 12:26:08 -0400

    add logging

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ chmod u+x Scripts/IBController.sh
 
 tail -F /tws/**/ibgateway.log &
 
-/usr/bin/xvfb-run --listen-tcp --server-num 99 -s "-auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24 -listen tcp" Scripts/IBController.sh 1023 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
+/usr/bin/xvfb-run -e /tmp/xvbf.log --listen-tcp --server-num 99 -s "-auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24 -listen tcp" Scripts/IBController.sh 1023 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
