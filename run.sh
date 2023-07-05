10fceae448b1b89f66c0a2b082599bbff4fc4edc
TheGreatGooo <thegreatgooo@gmail.com>
2023-07-05 11:43:58 -0400

    fix port

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ chmod u+x Scripts/IBController.sh
 
 tail -F /tws/**/ibgateway.log &
 
-/usr/bin/xvfb-run -s ":9999 -auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24" Scripts/IBController.sh 1023 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
+/usr/bin/xvfb-run -s ":99 -auth /tmp/xvfb.auth -ac -screen 0 1920x1080x24" Scripts/IBController.sh 1023 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
